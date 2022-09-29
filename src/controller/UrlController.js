acbc7ab0f401f0fd1ede3a06b22aec1b1af5dc65
shishir sharma <shishirsharma453@gmail.com>
2022-09-29 16:14:08 +0530

    hi

--- src/controller/UrlController.js
+++ src/controller/UrlController.js
@@ -1,28 +1,33 @@
 const urlModel = require("../model/UrlModel")
-const urlid = require("shortid")
+const shortid = require("shortid")
 const validUrl = require("valid-url")
 
 const urlcreation = async function(req,res){
+
     try{
+    
+        data = req.body
+    longUrl = data.longUrl
 
-    longUrl = req.body.longUrl
-
+    if(Object.keys(data).length === 0)return res.status(400).send({status:false,message:"data is required"})
+    
     if(!longUrl)return res.status(400).send({status:false, message:"wrong longUrl"})
 
     if(!validUrl.isUri(longUrl))return res.status(400).send({status:false,message:"longUrl is not valid"})
 
-    const urlexist = await urlModel.findOne({longUrl:longUrl})
-    // if(urlexist)return res.status(400).send({status:false,message:"longUrl already exist"})
- 
-    const baseUrl = "http://localhost:3000"
-    const urlCode = urlid.generate()
-    const obj = {
-        longUrl:longUrl,
-         urlCode : urlCode.toLowerCase(),
-         shortUrl : baseUrl+"/"+urlCode
-    }
+    const urlexist = await urlModel.findOne({longUrl:longUrl}).select({_id:0,longUrl:1,urlCode:1,shortUrl:1})
+    if(urlexist)return res.status(400).send({status:false,message:"longUrl already exist",data:urlexist})
+
+    const urlCode = shortid.generate().toLowerCase()
+
+    const baseUrl = "http://localhost:3000/"
     
-    const createUrl = await urlModel.create(obj)
+    const obj = {
+        longUrl: longUrl,
+        shortUrl : baseUrl+urlCode, 
+        urlCode: urlCode 
+    }
+    const createUrl = await  urlModel.create(obj)
     return res.status(201).send({status:true,message:"url created successfully",data:createUrl})
 
 }catch(err){
@@ -31,4 +36,15 @@ const urlcreation = async function(req,res){
     
 }
 
-module.exports= {urlcreation}
+
+
+const geturl = async function(req,res){
+    let urlCode = req.params.urlCode
+
+    let orignalUrl = await urlModel.findOne({urlCode:urlCode}).select({_id:0,longUrl:1})
+   
+
+    return res.status(302).redirect(orignalUrl.longUrl)
+}
+
+module.exports= {urlcreation,geturl}
--- src/route/route.js
+++ src/route/route.js
@@ -1,9 +1,10 @@
 const { Router } = require("express")
 const express = require("express")
 const route = express.Router()
-const{urlcreation} = require("../controller/UrlController")
+const{urlcreation,geturl} = require("../controller/UrlController")
 
 route.post("/url/shorten",urlcreation)
+route.get("/:urlCode",geturl)
 route.all("/*",function(req,res){
     return res.status(400).send({status:false,message:"invalid request"})
 })
