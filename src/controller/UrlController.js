82747c7d1ce5a3e83af94c813216bd00f54a5856
shishir sharma <shishirsharma453@gmail.com>
2022-09-30 11:39:07 +0530

    hello

--- src/controller/UrlController.js
+++ src/controller/UrlController.js
@@ -1,50 +1,63 @@
 const urlModel = require("../model/UrlModel")
 const shortid = require("shortid")
-const validUrl = require("valid-url")
+const validator = require("validator")
 
 const urlcreation = async function(req,res){
 
     try{
     
         data = req.body
-    longUrl = data.longUrl
+    const  longUrl = data.longUrl
 
     if(Object.keys(data).length === 0)return res.status(400).send({status:false,message:"data is required"})
     
-    if(!longUrl)return res.status(400).send({status:false, message:"wrong longUrl"})
+    if(!longUrl)return res.status(400).send({status:false, message:"please enter longUrl"})
 
-    if(!validUrl.isUri(longUrl))return res.status(400).send({status:false,message:"longUrl is not valid"})
+    if(typeof longUrl !== "string")return res.status(400).send({status:false,message:"url should be in string format"})
+
+    if(!validator.isURL(longUrl))return res.status(400).send({status:false,message:"longUrl is not a valid Url"})
 
     const urlexist = await urlModel.findOne({longUrl:longUrl}).select({_id:0,longUrl:1,urlCode:1,shortUrl:1})
-    if(urlexist)return res.status(400).send({status:false,message:"longUrl already exist",data:urlexist})
+    if(urlexist)return res.status(200).send({status:false,message:"longUrl already exist",data:urlexist})
 
     const urlCode = shortid.generate().toLowerCase()
 
-    const baseUrl = "http://localhost:3000/"
+    const baseUrl = "http://localhost:3000"
     
     const obj = {
         longUrl: longUrl,
-        shortUrl : baseUrl+urlCode, 
+        shortUrl : baseUrl+"/"+urlCode, 
         urlCode: urlCode 
     }
     const createUrl = await  urlModel.create(obj)
     return res.status(201).send({status:true,message:"url created successfully",data:obj})
 
 }catch(err){
-    res.status(500).send({status:false,error:err.message})
+    res.status(500).send({status:false,message:"server error",error:err.message})
 }
     
 }
 
-
-
 const geturl = async function(req,res){
+    try{
     let urlCode = req.params.urlCode
 
+    if(urlCode == ":urlCode"){
+        return res.status(400).send({status:false,message:"please enter urlcode in path param"})
+    }
+    if(/.*[A-Z].*/.test(urlCode)){
+        return res.status(400).send({ status: false, message: "please Enter urlCode only in lowercase" })
+    }
+    const urlCodeexist = await urlModel.findOne({urlCode:urlCode})
+    if(!urlCodeexist)return res.status(404).send({status:false,message:"urlcode not found"})
+
     let orignalUrl = await urlModel.findOne({urlCode:urlCode}).select({_id:0,longUrl:1})
-   
 
     return res.status(302).redirect(orignalUrl.longUrl)
+
+}catch(err){
+    return res.status(500).send({status:false,message:"server error",error:err.message})
+}
 }
 
 module.exports= {urlcreation,geturl}
--- src/route/route.js
+++ src/route/route.js
@@ -1,4 +1,4 @@
-const { Router } = require("express")
+
 const express = require("express")
 const route = express.Router()
 const{urlcreation,geturl} = require("../controller/UrlController")
@@ -6,7 +6,7 @@ const{urlcreation,geturl} = require("../controller/UrlController")
 route.post("/url/shorten",urlcreation)
 route.get("/:urlCode",geturl)
 route.all("/*",function(req,res){
-    return res.status(400).send({status:false,message:"invalid request"})
+    return res.status(400).send({status:false,message:"path not found"})
 })
 
 
